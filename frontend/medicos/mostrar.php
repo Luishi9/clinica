8b349d454e63e691444ba7a0a6069bfd5dd731c3
Luishi9 <luisrubin977@gmail.com>
2024-12-05 22:26:18 -0600

    iconos

--- frontend/medicos/mostrar.php
+++ frontend/medicos/mostrar.php
@@ -207,22 +207,53 @@ if (!isset($_SESSION['rol']) || ($_SESSION['rol'] != 1 && $_SESSION['rol'] != 3)
                                             </td>
 
                                             <td>
-                                                <a title="Actualizar" href="../medicos/editar.php?id=<?php echo $d->idodc ?>" class="btn btn-outline-primary">Actualizar</a>
-                                                <a title="Información" href="../medicos/info.php?id=<?php echo $d->idodc ?>" class="btn btn-outline-warning">Informacion</a>
-
+                                                <a title="Actualizar" href="../medicos/editar.php?id=<?php echo $d->idodc ?>" class="btn btn-outline-primary btn-sm">
+                                                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="icon icon-tabler icons-tabler-outline icon-tabler-edit">
+                                                        <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                                                        <path d="M7 7h-1a2 2 0 0 0 -2 2v9a2 2 0 0 0 2 2h9a2 2 0 0 0 2 -2v-1" />
+                                                        <path d="M20.385 6.585a2.1 2.1 0 0 0 -2.97 -2.97l-8.415 8.385v3h3l8.385 -8.415z" />
+                                                        <path d="M16 5l3 3" />
+                                                    </svg>
+                                                    Actualizar
+                                                </a>
+                                                <a title="Información" href="../medicos/info.php?id=<?php echo $d->idodc ?>" class="btn btn-outline-warning btn-sm">
+                                                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="icon icon-tabler icons-tabler-outline icon-tabler-help-octagon">
+                                                        <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                                                        <path d="M12.802 2.165l5.575 2.389c.48 .206 .863 .589 1.07 1.07l2.388 5.574c.22 .512 .22 1.092 0 1.604l-2.389 5.575c-.206 .48 -.589 .863 -1.07 1.07l-5.574 2.388c-.512 .22 -1.092 .22 -1.604 0l-5.575 -2.389a2.036 2.036 0 0 1 -1.07 -1.07l-2.388 -5.574a2.036 2.036 0 0 1 0 -1.604l2.389 -5.575c.206 -.48 .589 -.863 1.07 -1.07l5.574 -2.388a2.036 2.036 0 0 1 1.604 0z" />
+                                                        <path d="M12 16v.01" />
+                                                        <path d="M12 13a2 2 0 0 0 .914 -3.782a1.98 1.98 0 0 0 -2.414 .483" />
+                                                    </svg>
+                                                    Informacion
+                                                </a>
 
                                                 <form onsubmit="return confirm('Realmente desea eliminar el registro?');" method='POST' action='<?php $_SERVER['PHP_SELF'] ?>'>
                                                     <input type='hidden' name='idodc' value="<?php echo  $d->idodc; ?>">
 
-                                                    <button name='delete_patients' style="cursor: pointer;" class="btn btn-outline-danger"><span class="">Eliminar</span></button>
+                                                    <button name='delete_patients' style="cursor: pointer;" class="btn btn-outline-danger btn-sm">
+                                                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="icon icon-tabler icons-tabler-outline icon-tabler-trash-x">
+                                                            <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                                                            <path d="M4 7h16" />
+                                                            <path d="M5 7l1 12a2 2 0 0 0 2 2h8a2 2 0 0 0 2 -2l1 -12" />
+                                                            <path d="M9 7v-3a1 1 0 0 1 1 -1h4a1 1 0 0 1 1 1v3" />
+                                                            <path d="M10 12l4 4m0 -4l-4 4" />
+                                                        </svg>
+                                                        Eliminar
+                                                    </button>
                                                 </form>
 
                                                 <?php
                                                 if ($d->rol == '3') {
                                                     // code...
-                                                    echo '<a title="Cambiar contraseña"  href="../medicos/password.php?id=' . $d->idodc . '" class="fa fa-key"></a>';
+                                                    echo '<a title="Cambiar contraseña"  href="../medicos/password.php?id=' . $d->idodc . '" class="btn btn-outline-secondary btn-sm">
+                                                        <svg  xmlns="http://www.w3.org/2000/svg"  width="24"  height="24"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  
+                                                        stroke-width="2"  stroke-linecap="round"  stroke-linejoin="round"  class="icon icon-tabler icons-tabler-outline icon-tabler-key">
+                                                        <path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M16.555 3.843l3.602 3.602a2.877 2.877 0 0 1 0 4.069l-2.643 2.643a2.877 2.877 0 0 1 -4.069 0l-.301 -.301l-6.558 6.558a2 2 0 0 1 -1.239 .578l-.175 .008h-1.172a1 1 0 0 1 -.993 -.883l-.007 -.117v-1.172a2 2 0 0 1 .467 -1.284l.119 -.13l.414 -.414h2v-2h2v-2l2.144 -2.144l-.301 -.301a2.877 2.877 0 0 1 0 -4.069l2.643 -2.643a2.877 2.877 0 0 1 4.069 0z" /><path d="M15 9h.01" /></svg>    
+                                                    
+                                                        </a>';
                                                 } else {
-                                                    echo '<a title="Crear perfil" href="../medicos/crear.php?id=' . $d->idodc . '" class="fa fa-user-plus"></a>';
+                                                    echo '<a title="Crear perfil" href="../medicos/crear.php?id=' . $d->idodc . '" class="btn btn-outline-success btn-sm">
+                                                        <svg  xmlns="http://www.w3.org/2000/svg"  width="24"  height="24"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  stroke-width="2"  stroke-linecap="round"  stroke-linejoin="round"  class="icon icon-tabler icons-tabler-outline icon-tabler-user-plus"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M8 7a4 4 0 1 0 8 0a4 4 0 0 0 -8 0" /><path d="M16 19h6" /><path d="M19 16v6" /><path d="M6 21v-2a4 4 0 0 1 4 -4h4" /></svg>
+                                                    </a>';
                                                 }
 
                                                 ?>
